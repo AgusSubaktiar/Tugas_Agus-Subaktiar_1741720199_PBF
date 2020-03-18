2f6d6465f7a28185d8c32a885e419dfd02388a8e
Agus Subag <456jojon>
2020-03-18 13:50:50 +0700

    Update index.js

--- pertemuan7_redux/src/index.js
+++ pertemuan7_redux/src/index.js
@@ -6,6 +6,7 @@ import MainReducer from './reducer/MainReducer'
 import App from './App';
 import Table from './containers/Table'
 import CreateTodo from './containers/CreateTodo'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import registerServiceWorker from './registerServiceWorker';
 import * as serviceWorker from './serviceWorker';
