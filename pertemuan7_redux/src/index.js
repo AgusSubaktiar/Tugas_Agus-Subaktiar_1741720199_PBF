36e76c8df47d149bd8382912383fb896b2a6b92d
Agus Subag <456jojon>
2020-03-18 12:59:46 +0700

    tugas pertemuan 7

---
+++ pertemuan7_redux/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux' 
+import MainReducer from './reducer/MainReducer'
+import App from './App';
+import Table from './containers/Table'
+import CreateTodo from './containers/CreateTodo'
+
+// import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
+
+
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => 
+f)(createStore)(MainReducer)
+
+ReactDOM.render(<Provider store={store}> 
+<Table/>
+</Provider>
+, document.getElementById('root')); 
+// registerServiceWorker();
+serviceWorker.unregister();
