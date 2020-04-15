7a4ac7542cef9749a328a166aa0fd225c3edfd20
Agus Subag <456jojon>
2020-04-15 17:23:08 +0700

    React Firebase pratikum&tugas

---
+++ pertemuan11-firebase/src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyCq-hVeNqtbJA6--ubV9pMyH5NyEXXRgM0",
+    authDomain: "agussubaktiar-pbf-3076a.firebaseapp.com",
+    databaseURL: "https://agussubaktiar-pbf-3076a.firebaseio.com",
+    projectId: "agussubaktiar-pbf-3076a",
+    storageBucket: "agussubaktiar-pbf-3076a.appspot.com",
+    messagingSenderId: "19447465681",
+    appId: "1:19447465681:web:9ab58e0ca927893dbea7f0",
+    measurementId: "G-QNNQRKC4FP"
+  };
