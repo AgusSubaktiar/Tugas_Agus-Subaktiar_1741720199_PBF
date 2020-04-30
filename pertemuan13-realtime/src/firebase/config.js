eda1687dc7c03f793721f1881735605b830f0604
Agus Subag <456jojon>
2020-04-30 11:52:52 +0700

    firebase realtime

---
+++ pertemuan13-realtime/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBaR9L2ndZvKjwmjf1e67VEZdajGBc4SP4",
+    authDomain: "agusloginpbf-14c42.firebaseapp.com",
+    databaseURL: "https://agusloginpbf-14c42.firebaseio.com",
+    projectId: "agusloginpbf-14c42",
+    storageBucket: "agusloginpbf-14c42.appspot.com",
+    messagingSenderId: "990224233829",
+    appId: "1:990224233829:web:ec5ce4c66effc653c95d8c",
+    measurementId: "G-6X9PP9QCSN"
+  };
+
+  export default firebaseConfig;
